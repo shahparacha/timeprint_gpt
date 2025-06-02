f83ef088f464614ba7c794a46eb51771d8a71ff5
SP <shah.paracha@gmail.com>
2025-06-02 12:32:11 -0400

    fixes needed temp

--- app/components/HeaderWrapper.tsx
+++ app/components/HeaderWrapper.tsx
@@ -1,8 +1,8 @@
-import { auth } from "@clerk/nextjs/server";
+//import { auth } from "@clerk/nextjs/server"; put back when we have a real project
 import Header from "./Header";
 import { Metadata } from "next";
 
 export default async function HeaderWrapper() {
-  const { userId } = await auth();
-  return <Header userId={userId} />;
+  //const { userId } = await auth(); put back when we have a real project
+  //return <Header userId={userId} />; put back when we have a real project
 }
