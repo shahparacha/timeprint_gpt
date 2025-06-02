431a6d00c15e6b65a6e2638331ae7642eda3af07
SP <shah.paracha@gmail.com>
2025-06-02 12:38:04 -0400

    fix temp

--- app/components/HeaderWrapper.tsx
+++ app/components/HeaderWrapper.tsx
@@ -4,5 +4,5 @@ import { Metadata } from "next";
 
 export default async function HeaderWrapper() {
   //const { userId } = await auth(); put back when we have a real project
-  //return <Header userId={userId} />; put back when we have a real project
+  return <Header userId={null} />; // <-- Pass null for now
 }
